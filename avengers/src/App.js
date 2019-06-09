4047ac9afbb1afcb762c3bb53aaef9491467f930
abidex4yemi <abidex4yemi@gmail.com>
2019-06-09 14:28:27 +0100

    Create home component

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { AvengersList } from './components/AvengersList';
 import './App.css';
 
 function App() {
-	return <div className="App">welcome</div>;
+	return (
+		<div className="App">
+			<Route exact path="/" component={Home} />
+			<Route exact path="/avengers" component={AvengersList} />
+		</div>
+	);
 }
 
 export default App;
---
+++ avengers/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home = () => {
+	return (
+		<div>
+			<h1>Avengers Database</h1>
+			<h4>Danger! This is classified information</h4>
+		</div>
+	);
+};
